07e1847b97994c1f2fa51a07a163e018484616d3
Joel Hammond-Turner <joel@hammond-turner.org.uk>
2021-11-18 17:15:25 +0000

    Rename projects

--- 01-LegacyOfGlobalEvil/WinformsReloaded/Properties/Resources.Designer.cs
+++ 01-LegacyOfGlobalEvil/WinformsReloaded/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WinformsReloaded.Properties {
+namespace Chapter_1_-_WinformsReloaded.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WinformsReloaded.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinformsReloaded.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Chapter_1_-_WinformsReloaded.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
