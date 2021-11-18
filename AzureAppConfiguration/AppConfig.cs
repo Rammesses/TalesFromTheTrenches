9832bfd93f78f56ce54d223bb38544320a0d07da
Joel Hammond-Turner <joel@hammond-turner.org.uk>
2021-11-18 17:11:49 +0000

    Add AzureAppConfiguration demo

---
+++ AzureAppConfiguration/AppConfig.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class AppConfig
+{
+    public string AppName { get; set; }
+    public string AppVersion { get; set; }
+
+    public override string ToString()
+    {
+        return $"{AppName} v{AppVersion}";
+    }
+}
---
+++ AzureAppConfiguration/ConfigPrinterService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AzureAppConfiguration
+{
+    public class ConfigPrinterService<T> : IHostedService
+    {
+        private readonly IOptionsMonitor<T> _configMonitor;
+        private readonly ILogger _logger;
+        private readonly Timer _timer;
+
+        public ConfigPrinterService(IOptionsMonitor<T> monitor, ILogger<ConfigPrinterService<T>> logger)
+        {
+            _configMonitor = monitor ?? throw new ArgumentNullException(nameof(monitor));
+            logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            _timer = new Timer(DumpConfig);
+
+            monitor.OnChange(config =>
+            {
+                _logger.LogCritical("Config changed!");
+            });
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            // start the timer
+            _timer.Change(TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        private void DumpConfig(object state)
+        {
+            Console.WriteLine($"Configuration: {_configMonitor.CurrentValue}");
+        }
+    }
+}
